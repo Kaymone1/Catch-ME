ad87b2d8cf1892dbcb50c876be90fca52fb0e903
Kaymone1 <kennya.benoit15@gmail.com>
2023-08-05 18:41:06 -0400

    editing java variables

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 
 const timeLeft = document.querySelector('#time');
+let timer = 60;
 
 const instrHolder = document.getElementById('howtoplay');
 
@@ -10,17 +11,26 @@ instrBtn.addEventListener(`click`, () => {
     console.log("button clicked")
 });
 
-const quitBtn = document.getElementById('pressQuit');
-quitBtn.addEventListener('click', () => {
-    console.log('click works')
-})
+// const quitBtn = document.getElementById('pressQuit');
+
+// quitBtn.addEventListener('click', () => {
+    // let quitter = prompt("Are you sure you want to quit?")
+    // const answer = () => {
+    //     if(quitter === 'yes') {
+
+    // }
+//     console.log('click works')
+// })
 
 const score = document.querySelector('#score');
 let points = 0
 
 const pika = document.querySelector('.pikaRun');
+let speed = 1800; //initial speed
 
 const squares = document.querySelectorAll('.square');
+
+// let hitSpot = null
 
 function randomSquare() {
     squares.forEach(square => {
@@ -32,3 +42,9 @@ function randomSquare() {
     console.log(quickAttack)
 }
 randomSquare()
+
+// function pikaMove() {
+//     let timerId = null
+//     timerId = setInterval(randomSquare, 1000)
+// }
+// pikaMove()
