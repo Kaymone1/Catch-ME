a9358fc91827d59e934c60d8d972ac468496d72d
Kaymone1 <kennya.benoit15@gmail.com>
2023-08-09 20:25:16 -0400

    variables done for now

--- js/app.js
+++ js/app.js
@@ -1,10 +1,23 @@
 // Get the score and time elements
-const scoreElement = document.getElementById('score');
-const timeElement = document.getElementById('time');
+const scoreHlder = document.getElementById('score');
+const timeHlder = document.getElementById('time');
 
 // Get the start and quit buttons
-const startButton = document.getElementById('startbutton');
-const quitButton = document.getElementById('pressQuit');
+const startBtn = document.getElementById('startbutton');
+const quitBtn = document.getElementById('pressQuit');
 
 // Get all the squares so I can manipulate them
-const squares = document.querySelectorAll('.square');
+const sqrs = document.querySelectorAll('.square');
+
+// initialize variables the fun begins
+//I want to track players score/time need a variable
+let score = 0;
+let timeLeft = 60; //seconds timer
+//players will begin at 0pts this reps who is going at the time
+let currentPlyer = 0;
+//tracking pikachus movements; this will be what i manipulate to change his speed
+let pikaSpeed = null;
+
+// starting pika speed in 4.5 seconds? will be testig time to see
+//what time intervals would be best to start pikas movements
+let pikaStart = 4500; 
