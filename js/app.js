edc13109de8719e95a857b2757b0911c1c77a395
Kaymone1 <kennya.benoit15@gmail.com>
2023-08-09 22:52:57 -0400

    game is functioning

--- js/app.js
+++ js/app.js
@@ -15,25 +15,31 @@ const sqrs = document.querySelectorAll('.square');
 // initialize variables the fun begins
 //I want to track players score/time need a variable
 let score = 0;
-let timeLeft = 60; //seconds timer
+let timeLeft = 30; //seconds timer
+
 //players will begin at 0pts this reps who is going at the time
 let currentPlyer = 0;
+
+// //creating a player turn element so i know which person is scoring
+// const playerTurn = document.getElementById('playerTurn') tbc....
+
+
 //tracking pikachus movements; this will be what i manipulate to change his speed
 let pikaMvmentTime = null;
 
 // starting pika speed in 4.5 seconds? will be testig time to see
 //what time intervals would be best to start pikas movements
-let pikaSpeed = 4500; 
+let pikaSpeed = 4000; 
 
 // Function to update the score, i created the score board with text so change text
 //I target score with textContent
 function upScore() {
-    scoreElement.textContent = score;
+    scoreHlder.textContent = score;
   }
 
 // Function to update the time
 function upTime() {
-    timeElement.textContent = timeLeft;
+    timeHlder.textContent = timeLeft;
   }
 
 // Function to handle a square click
@@ -43,11 +49,11 @@ function squareClick(event) {
     //condition of what happens when player clicks pikaRun; target will be .pikaRun
     // classlist to bring the domtokenlist of pikarun (method i want to interact with)
     //I am checking that only img in square is clicked
-    if (event.target.classList.contains('.pikaRun')) { 
+    if (event.target.classList.contains('pikaRun')) { 
       score += 1;
       console.log('pikachu has been caught')
-      updateScore();
-      event.target.classList.remove('.pikaRun');
+      upScore();
+      event.target.classList.remove('pikaRun');
       pikaMove();
     }
   }
@@ -55,17 +61,66 @@ function squareClick(event) {
 // Function to move pika
 function pikaMove() {
     // Remove pikaRun class from all squares
-    squares.forEach(square => square.remove(pika)); 
+    sqrs.forEach(square => square.classList.remove('pikaRun'));
   
     // Randomize a square to place pika everytime he moves
-    const randomIndex = Math.floor(Math.random() * squares.length);
-    const randomSquare = squares[randomIndex];
-    randomSquare.add(pika); 
+    const randomIndex = Math.floor(Math.random() * sqrs.length);
+    const randomSquare = sqrs[randomIndex];
+    randomSquare.classList.add('pikaRun');
   
     // Speed up pikas movement
     pikaSpeed -= 10;
     clearInterval(pikaMvmentTime);
-    moleIntervalId = setInterval(pikaMove, pikaSpeed);
-    console.log("pika moving faster")
-  }
-  
+    pikaMvmentTime = setInterval(pikaMove, pikaSpeed);
+    console.log("pika moving faster");
+}
+
+  // Function to start the game
+function startGame() {
+    score = 0;
+    timeLeft= 40;
+    currentPlyer = 0;
+    pikaSpeed = 1000;
+    upScore();
+    upTime();
+
+// Clear existing interval (if any)
+clearInterval(pikaMvmentTime);
+
+// Set up the game timer interval
+const timerIntervalId = setInterval(() => {
+    if (timeLeft > 0) {
+      timeLeft -= 1;//go down by 1 second
+      upTime();
+    } else {
+      clearInterval(timerIntervalId);
+      clearInterval(pikaMvmentTime);
+      announceWinner();
+    }
+  }, 1000);
+
+  pikaMove();
+}
+
+// Function to announce the winner
+function announceWinner() {
+    const winner = score > 1 ? currentPlyer : 1;
+    alert(`Player ${winner} wins with a score of ${score}!`);
+    
+}
+
+  // Attach click event listeners to squares
+sqrs.forEach(square => square.addEventListener('click', squareClick));
+
+// Attach click event listener to the "Start" button
+startBtn.addEventListener('click', () => {
+  startGame();
+});
+
+// Attach click event listener to the "Quit" button
+quitBtn.addEventListener('click', () => {
+  clearInterval(pikaMvmentTime);
+  announceWinner();
+  startGame()
+    console.log('game reset')
+});
