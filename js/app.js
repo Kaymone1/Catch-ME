3c7ed0dfc0d96b0776fe5a1a1555e05a70c866f9
Kaymone1 <kennya.benoit15@gmail.com>
2023-08-09 21:45:15 -0400

    pika's moves function

--- js/app.js
+++ js/app.js
@@ -19,11 +19,11 @@ let timeLeft = 60; //seconds timer
 //players will begin at 0pts this reps who is going at the time
 let currentPlyer = 0;
 //tracking pikachus movements; this will be what i manipulate to change his speed
-let pikaSpeed = null;
+let pikaMvmentTime = null;
 
 // starting pika speed in 4.5 seconds? will be testig time to see
 //what time intervals would be best to start pikas movements
-let pikaStart = 4500; 
+let pikaSpeed = 4500; 
 
 // Function to update the score, i created the score board with text so change text
 //I target score with textContent
@@ -43,12 +43,29 @@ function squareClick(event) {
     //condition of what happens when player clicks pikaRun; target will be .pikaRun
     // classlist to bring the domtokenlist of pikarun (method i want to interact with)
     //I am checking that only img in square is clicked
-    if (event.target.classList.contains('pikaRun')) { 
+    if (event.target.classList.contains('.pikaRun')) { 
       score += 1;
       console.log('pikachu has been caught')
       updateScore();
-      event.target.classList.remove('pikaRun');
+      event.target.classList.remove('.pikaRun');
       pikaMove();
     }
   }
 
+// Function to move pika
+function pikaMove() {
+    // Remove pikaRun class from all squares
+    squares.forEach(square => square.remove(pika)); 
+  
+    // Randomize a square to place pika everytime he moves
+    const randomIndex = Math.floor(Math.random() * squares.length);
+    const randomSquare = squares[randomIndex];
+    randomSquare.add(pika); 
+  
+    // Speed up pikas movement
+    pikaSpeed -= 10;
+    clearInterval(pikaMvmentTime);
+    moleIntervalId = setInterval(pikaMove, pikaSpeed);
+    console.log("pika moving faster")
+  }
+  
